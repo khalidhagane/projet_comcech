52530d54105da216732801e7afc237e810c2ae46
khalid hagane <khalidhagane62@gmail.com>
2022-05-29 12:40:31 +0100

    ajoute navbar, et travial en page d'accueil

---
+++ header.php
@@ -0,0 +1,101 @@
+<?php
+require_once 'hed.php';
+require_once 'header.php';
+?>
+<body>
+<main class="vh-100 ">
+    <nav class="px-4 py-1 navbar bg-info d-flex flex-row justify-content-between px-2">
+        <div class="d-flex flex-row gap-3">
+            <div>
+                <i class="fas fa-list-alt"></i>
+                <span>produit</span>
+            </div>
+            
+            <div class="">
+                <i class="fas fa-search"></i>
+                <span>recherche</span>
+                
+            </div>
+        </div>
+
+        <div class="d-flex flex-row justify-content-center w-25  h-25" >
+            <img class="w-25  h-25" src="images/log.png" alt="logo">
+            
+        </div>
+        
+
+        <div class="d-flex flex-row  gap-3">
+            <div class=>
+                <i class="fas fa-user"></i>
+                <span>login</span>
+            </div>
+            
+            <div>
+                <i class="fas fa-shopping-cart"></i>
+                <span>panier</span>
+            </div>
+        </div>
+
+</nav> 
+
+    
+
+<!-- <div class="collapse" id="navbarToggleExternalContent">
+  <div class="bg-dark p-4">
+    <h5 class="text-white h4">Collapsed content</h5>
+    <span class="text-muted">Toggleable via the navbar brand.</span>
+  </div>
+</div>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+
+  <div class="collapse" id="navbarToggleExternalContent">
+  <div class="bg-dark p-4">
+    <h5 class="text-white h4">Collapsed content</h5>
+    <span class="text-muted">Toggleable via the navbar brand.</span>
+  </div>
+</div>
+
+  
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <a class="navbar-brand" href="#">Navbar</a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link disabled" href="#"  aria-disabled="true">Disabled</a>
+        </li> -->
+      <!-- </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav> --> 
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+require_once 'hed.php';
+require_once 'header.php';
+?>
+
+
+    
+
+    
+    <div class="image_daccueil h-100 d-flex flex-row justify-content-center align-items-center">
+       
+        <div class="h-50 w-50 d-flex flex-column gap-3 justify-content-center align-items-center text-center ">
+            <div>
+                <h1 class="text-white py-2">Location de matériel pour les professionnels</h1>
+                <p class="text-white col-12">Louez le matériel professionnel qu’il vous faut, quand il faut, et où il faut !</p>
+            </div>
+                <form class=" col-10 mx-auto d-flex bg-white rounded-pill border-0" role="search">
+                    <input class="form-control me-2 rounded-pill border-0" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn  rounded-pill search-btn border-white text-secondary" type="submit"><i class="fa fa-search "></i></button>
+                </form>
+        </div>
+    </div>
+
+    <div class=" d-flex flex-column align-items-center " >
+        <div class="w-50 d-flex flex-column align-items-center ">
+            <h1 class="color_comcech  ">Le remises</h1>
+            <p class="text-center">Merci pour le téléchargement: Modèle de bannière de publication de médias sociaux de promotion de location de voiture Psd Premium</p>
+        </div>
+        <div>
+            <div class="card h-25 w-50 d-flex flex-row">
+                <img class="h-25 w-50" src="images/groupe.jpeg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vitae iure eligendi.</p>
+            </div>
+            
+        </div>
+    </div>
+
+    
+</main>
