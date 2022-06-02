885f1671c548d3433e648130e3c74c61b5930ede
khalid hagane <khalidhagane62@gmail.com>
2022-06-02 15:06:03 +0100

    ajoute page statistique de produit

---
+++ one-produit.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'hed.php';
+require_once 'header.php';
+?>
+<div class=" container w-100 h-100 d-flex  justify-content-center align-items-center">
+    <div class=" card w-md-50 w-lg-50 h-75 w-sm-100  d-flex  flex-row p-3 ">
+        <div class="h-50   d-flex flex-column  align-items-center">
+            <h1 class="w-50  " >groupe</h1>
+            <p class="w-50  " >groupe 5000 V</p>
+            <img class="w-50  " src="images/groupe.png" alt="photo">
+        </div>
+        <div class=" ">
+            <div>
+            <h1 class="">caractiristique</h1>
+            </div>
+            <span>autonomie :</span><span> 6.40 KVA</span><br>
+            <span>puissance :</span><span> 16 l</span><br>
+            <span>capacité :</span><span> 12 l</span><br>
+            <span>consomation :</span><span> 12 l/h</span><br>
+            <span>sortie alternatif :</span><span> 12h</span><br>
+            <span>autonomie :</span><span> 6.40 KVA</span><br>
+            <span>puissance :</span><span> 16 l</span><br>
+            <span>capacité :</span><span> 12 l</span><br>
+            <span>consomation :</span><span> 12 l/h</span><br>
+            <span>sortie alternatif :</span><span> 12h</span><br>
+            <div>
+            <span class="">tarif : </span><span> 12h</span><br>
+            </div>
+            <div class="py-2 d-flex  justify-content-center">
+            <button type="ACHETE" class="btn btn-border-comcech rounded-pill px-5 ">ACHETE</button>
+            </div>
+            
+        </div>
+    </div>
+</div>
