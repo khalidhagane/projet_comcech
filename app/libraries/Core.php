76c85c4aa3a0974c2be20416af7a64ba8a25d1d1
khalid hagane <khalidhagane62@gmail.com>
2022-06-07 09:03:42 +0100

    ajoute page dashbord et page panie

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -14,6 +14,7 @@
 
       $url = $this->getUrl();
       // Look in controllers for first value
+      // file_exists kat man hadak controller wach kain ola la. // ucwords function kat khali liya dima lharf 1 man kalima kbiiir 
       if((isset($url[1])) && file_exists('../app/controllers/' . ucwords($url[0]). '.php')){
         // If exists, set as controller
         $this->currentController = ucwords($url[0]);
@@ -46,9 +47,9 @@
 
     public function getUrl(){
       if(isset($_GET['url'])){
-        $url = rtrim($_GET['url'], '/');
-        $url = filter_var($url, FILTER_SANITIZE_URL);
-        $url = explode('/', $url);
+        $url = rtrim($_GET['url'], '/'); // rtrim function kat 9bal 2 dyal li parametr string o o hadik lhaja li bghiit n7ayadha 
+        $url = filter_var($url, FILTER_SANITIZE_URL); //  filter_var function kat 9bal 2 dyal parametr string o method pré diffiné kat filtri liya man caracter
+        $url = explode('/', $url); // explode function kat 9bal 2 dyal li parametr string o hadak caracter bach bghitiha tfra9 lik string otkhazzanha f array 
         return $url;
       }
     }
